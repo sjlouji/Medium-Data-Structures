82ed7714ec542c9599521b857d6dd2e72069fd60
sjlouji <sjlouji10@gmail.com>
2023-05-23 11:21:15 +0530

    Adding time

--- linear-search/linear-search.py
+++ linear-search/linear-search.py
@@ -1,3 +1,5 @@
+import time
+
 def linearSearch(inputList, searchElement):
 	position = -1;
 	for i in range(0, len(inputList)):
@@ -8,10 +10,14 @@ def linearSearch(inputList, searchElement):
 	return position;
 
 inputList = [1,3,2,6,5,4,9,8,7]
-searchElement = 6
+searchElement = 1
 
 
+start = time.time()
 position = linearSearch(inputList, searchElement)
+end = time.time()
+runtime = end - start
+print(runtime)
 
 if (position != -1):
 	print('Search Element found at position: ', position + 1)
